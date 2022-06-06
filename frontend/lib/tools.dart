651fff958dab456ea4dd46ed459bac6f4eb43135
Diego Imbert <diegoimbert@pm.me>
2022-06-07 01:22:34 +0200

    listToRec functio

--- frontend/lib/tools.dart
+++ frontend/lib/tools.dart
@@ -1,12 +1,27 @@
-class RecToList<T> {
-  RecToList(this.value, this.depth);
+class RecAsList<T> {
+  RecAsList(this.value, this.depth);
 
   final T value;
   final int depth;
 }
 
-Iterable<RecToList<T>> recToList<T>(T t, Iterable<T> Function(T) f,
+Iterable<RecAsList<T>> recToList<T>(T t, Iterable<T> Function(T) f,
     {int depth = 0}) sync* {
-  yield RecToList(t, depth);
+  yield RecAsList(t, depth);
   yield* f(t).expand((e) => recToList(depth: depth + 1, e, f));
+}
+
+Iterable<T> listToRec<T>(
+    Iterable<RecAsList<T>> xs, T Function(T, Iterable<T> newChildren) f) sync* {
+  if (xs.length <= 1) {
+    if (xs.isNotEmpty) yield xs.first.value;
+    return;
+  }
+  final first = xs.first;
+  final restIter = xs.skip(1).takeWhile((e) => e.depth > first.depth);
+  final restRec = listToRec(restIter, f);
+  yield f(first.value, restRec);
+  if (restIter.length + 1 < xs.length) {
+    yield* listToRec(xs.skip(restIter.length + 1), f);
+  }
 }
