f3e94db7eae2cb86eec4f84ab80bf296291c5404
Diego Imbert <diegoimbert@pm.me>
2022-06-07 01:38:13 +0200

    RAGEQUIT BITCH

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,4 @@
-import 'package:fireclock/task.dart';
-import 'package:fireclock/widgets/task_widget.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 void main() {
@@ -28,28 +25,6 @@ class FireClockApp extends HookConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final ValueNotifier<int?> selectedTaskId = useState(null);
-
-    final tasks = exampleTasks;
-    TaskItem toTaskItem(Task task, {int level = 0}) {
-      return TaskItem(
-        name: task.name,
-        color: taskColors[level % taskColors.length],
-        selected: selectedTaskId.value == task.id,
-        onSelect: () => selectedTaskId.value =
-            selectedTaskId.value == task.id ? null : task.id,
-        children: [
-          ...task.children.map((t) => toTaskItem(t, level: level + 1))
-        ],
-      );
-    }
-
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: Column(
-          children: [...tasks.map(toTaskItem)],
-        ),
-      ),
-    );
+    return Container();
   }
 }
